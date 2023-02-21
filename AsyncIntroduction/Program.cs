178db82a8d79fe7aec16369a3f3b573780633e37
Nedeljko Savic <nedeljko.savic.c@gmail.com>
2023-02-21 16:04:31 +0100

    Add project files.

---
+++ AsyncIntroduction/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncIntroduction
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("First line of text");
+
+        //    Thread.Sleep(3000);
+
+        //    Console.WriteLine("Second line of text");
+
+        //    Console.ReadKey();
+
+        //}
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("First line of text");
+
+            var task = DoSomeWork();
+
+            var result = task.Result;
+
+            Console.WriteLine("Second line of text");
+
+            //int resultOfDoSomeWork = await result;
+
+            Console.ReadKey();
+        }
+
+        static async Task<int> DoSomeWork()
+        {
+            Console.WriteLine("DoSomeWork - 1");
+
+            await Task.Delay(5000);
+
+            Console.WriteLine("DoSomeWork - 2");
+
+            return 5;
+        }
+    }
+}
---
+++ WinFormsAsync/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsAsync
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var task = DoSomeHeavyWork();
+            var task2 = DoSomeHeavyWork();
+
+            //await Task.Run(LongRunningMethod);
+
+            var task3 = Task.FromResult(123);
+
+            var valueOfTask3 = await task3;
+
+            textBox1.Text = "Returned from the task";
+
+            await task;
+        }
+
+        private async void Test()
+        {
+
+        }
+
+        private async Task DoSomethingOnSuccess(Task t)
+        {
+
+        }
+
+        private async Task DoSomethingOnError(Task t)
+        {
+
+        }
+
+        private void LongRunningMethod()
+        {
+            Thread.Sleep(10000);
+            MessageBox.Show("LongRunningMethod is done!");
+        }
+
+        private async Task DoSomeHeavyWork()
+        {
+            textBox1.Text = "Runing...";
+            await Task.Delay(5000);
+            textBox1.Text = "Work has completed!";
+
+
+            //throw new Exception("Exception has occurred");
+        }
+    }
+}
